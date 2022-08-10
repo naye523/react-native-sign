31fe4c0e6406c8cb32d92173d2dde5b6c6ac1da6
Ilja <idaderko@gmail.com>
2022-08-10 17:26:53 +0300

    save progress

--- android/src/main/java/walletconnect/sign/ReactNativeSignModule.kt
+++ android/src/main/java/walletconnect/sign/ReactNativeSignModule.kt
@@ -1,22 +1,35 @@
 package walletconnect.sign
 
+import android.util.Log
+
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+
+import com.walletconnect.sign.client.Sign
+import com.walletconnect.sign.client.SignClient
 
 class ReactNativeSignModule : Module() {
   override fun definition() = ModuleDefinition {
     Name("ReactNativeSign")
 
-    Events("onChange")
+    Events("session_proposal")
 
-    Function("hello") {
-      "Hello world! 👋"
-    }
+    Function("init") {
+        val options = Sign.Params.Init(
+          application = appContext.currentActivity!!.application,
+          connectionType = Sign.ConnectionType.AUTOMATIC,
+          relayServerUrl = "wss://",
+          metadata = Sign.Model.AppMetaData(
+            name = "Test App",
+            description = "Test Description",
+            url = "Tes utl",
+            icons = listOf("1", "2")
+          )
+        )
 
-    AsyncFunction("setValueAsync") { value: String ->
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
+      SignClient.initialize(options) { error ->
+        throw error.throwable
+      }
     }
 
   }
